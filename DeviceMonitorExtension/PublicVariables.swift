da1e6091bfc89c14f9bb825eb2e34fbafbe12323
Saamu02 <iusamairfan@gmail.com>
2024-07-03 12:55:34 +0500

    add missing file in the project

---
+++ DeviceMonitorExtension/PublicVariables.swift
@@ -0,0 +1,12 @@
+//
+//  PublicVariables.swift
+//  QuizlrTimeLimitExtension
+//
+//  Created by USER on 29/05/2023.
+//
+
+import Foundation
+
+struct PublicVariable {
+    public static var timeLimit = 0
+}
